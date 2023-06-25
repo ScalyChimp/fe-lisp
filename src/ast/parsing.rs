a9f2e413a3390a1957b03531dfff6c5bf814be74
scalychimp <scalychimp@gmail.com>
2023-06-25 21:51:54 +0100

    * src/ast/parsing.rs: tidy up

--- src/ast/parsing.rs
+++ src/ast/parsing.rs
@@ -5,12 +5,12 @@ use chumsky::Parser;
 pub fn parse_expr() -> impl Parser<char, Expr, Error = Simple<char>> {
     let positive_num = text::int(10).from_str::<i64>().unwrapped();
     let negative_num = just('-').then(positive_num).map(|x| Expr::Number(-x.1));
-    let bool = choice::<_, Simple<char>>((
-        text::keyword::<char, _, Simple<char>>("true").to(Expr::Bool(true)),
-        text::keyword::<char, _, Simple<char>>("false").to(Expr::Bool(false)),
+    let bool = choice((
+        text::keyword("true").to(Expr::Bool(true)),
+        text::keyword("false").to(Expr::Bool(false)),
     ));
 
-    let sym = filter::<char, _, Simple<char>>(|&c: &char| c != '(' && c != ')' && c != ' ')
+    let sym = filter(|&c: &char| c != '(' && c != ')' && c != ' ')
         .repeated()
         .at_least(1)
         .at_most(64)
